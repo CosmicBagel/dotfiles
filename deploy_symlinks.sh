0e381e56875287a11426db037844585da5e85473
CosmicBagel <>
2024-04-05 15:58:09 -0600

    added symlink for aconfmgr files

--- deploy_symlinks.sh
+++ deploy_symlinks.sh
@@ -7,6 +7,8 @@ symlinks_dic["$HOME/dotfiles/profile"]="$HOME/.profile"
 symlinks_dic["$HOME/dotfiles/bashrc"]="$HOME/.bashrc"
 symlinks_dic["$HOME/dotfiles/gitconfig"]="$HOME/.gitconfig"
 
+symlinks_dic["$HOME/dotfiles/aconfmgr"]="$HOME/.config/aconfmgr"
+
 symlinks_dic["$HOME/dotfiles/config/chromium-flags.conf"]="$HOME/.config/chromium-flags.conf"
 symlinks_dic["$HOME/dotfiles/config/kcminputrc"]="$HOME/.config/kcminputrc"
 symlinks_dic["$HOME/dotfiles/config/kglobalshortcutsrc"]="$HOME/.config/kglobalshortcutsrc"
