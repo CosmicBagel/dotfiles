9ddb4a881afea6c79a1033c62bfd80baef6b21c3
CosmicBagel <>
2024-04-05 18:33:00 -0600

    terminal leader key should no longer conflict with nvim

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -41,9 +41,10 @@ require('tabbar').apply_tab_bar_config(config)
 config.colors = require("colors")
 
 -- Key bindings
+config.debug_key_events = true
 config.enable_kitty_keyboard = true -- needed for neovim keybindings
 config.disable_default_key_bindings = true
-config.leader = { key = 'b', mods = 'CTRL', timeout_milliseconds = 1500 }
+config.leader = { key = 'b', mods = 'CMD', timeout_milliseconds = 1500 }
 config.keys = require("keys")
 
 return config
