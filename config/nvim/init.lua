669b556ba526db0641510de4bc5be4252c535594
CosmicBagel <>
2024-04-12 19:14:09 -0600

    more nvim config

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,23 +1,61 @@
 
 -- -- -- basic vim options -- -- --
-vim.cmd("set linebreak") -- don't spilt words when line wrapping
---vim.opt.showbreak = ">> "  -- show > when a line is split
-vim.opt.colorcolumn = "80"  -- displays guideline at 80 characters
-vim.opt.autowriteall = true -- kind of like auto-save
-vim.opt.number = true -- show line numbers
 vim.opt.clipboard = "unnamedplus" -- use system clipboard for default yank
 vim.opt.termguicolors = true -- enable proper colors
+vim.opt.autowriteall = false -- kind of like auto-save, but I only want it in certain situations
 vim.opt.undofile = true -- preserve undo state, needed for undotree
+
+vim.opt.number = true -- show line numbers
+vim.cmd("set linebreak") -- don't spilt words when line wrapping
+vim.opt.colorcolumn = "80"  -- displays guideline at 80 characters
+
+-- when backspacing, delete indents, end of lines, and starts(?)
 vim.opt.backspace = "indent,eol,start"
-vim.opt.expandtab = true
+
+-- keep 10 lines on screen above and below cursor
+vim.opt.scrolloff = 10
+
+-- sensible tab behaviour
+vim.opt.expandtab = true -- replace tabs with spaces
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 
+-- show the gutter 
+vim.opt.signcolumn = 'yes'
+
+-- time (ms) before swap is written to disk (defaults to 4000)
+vim.opt.updatetime = 250
+
+-- Configure how new splits should be opened
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+-- show offscreen substitions (:s :%s) in a preview pane
+vim.opt.inccommand = 'split'
+
+-- display undesireable characters (trailing space, tabs not converted to 
+-- spaces, and weird non-breaking spaces)
+vim.opt.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
+-- highlight line cursor is currently on
+vim.opt.cursorline = true
+
+-- Once a proper status line is installed, might want to remove this
+-- Don't show the mode, since it's already in the status line
+vim.opt.showmode = false
+
 -- -- -- mappings -- -- --
+-- Set <space> as the leader key
+--  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
 -- more sane movement, important when lines are wrapped
---vim.keymap.set('i', '<c-bs>', 'TEST INPUT SUCCESS', {noremap = true, silent = true})
 vim.keymap.set('n', 'j', 'gj', {noremap = true, silent = true})
 vim.keymap.set('n', 'k', 'gk', {noremap = true, silent = true})
+
+-- make ctrl+backspace work normally
 vim.keymap.set('i', '<c-bs>', '<c-w>', {noremap = true, silent = true})
 
 -- escape clears highlights and current message (in normal mode)
@@ -39,7 +77,35 @@ augroup END
 vim.api.nvim_create_user_command('CenterCursorDisable',
 function()
     vim.api.nvim_clear_autocmds({group = "VCenterCursor"})
-    vim.opt.scrolloff=0
+    vim.opt.scrolloff=10
 end, {})
 
-vim.cmd.CenterCursorEnable()
+-- vim.cmd.CenterCursorEnable()
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
+
+
+-- ==== plugin shopping list ==== --
+-- lazy.nvim
+-- telescope
+-- cmp
+-- some kind of status bar
+-- oil.nvim
+-- jump or sneak
+-- https://codeium.com/
+-- steal things from this guy! https://youtu.be/V070Zmvx9AM
+--      lots of little tricks in here
+--  more text objects (https://github.com/nvim-treesitter/nvim-treesitter-textobjects)
+-- https://github.com/epwalsh/obsidian.nvim
+
+
