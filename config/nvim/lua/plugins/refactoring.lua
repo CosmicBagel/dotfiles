56c0adec94334e9bdcb6c2764c9bdbcc7c2cc925
CosmicBagel <>
2024-04-18 15:33:23 -0600

    added refactoring pluging

---
+++ config/nvim/lua/plugins/refactoring.lua
@@ -0,0 +1,29 @@
+return {
+    "ThePrimeagen/refactoring.nvim",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-treesitter/nvim-treesitter",
+        "nvim-telescope/telescope.nvim",
+    },
+    config = function()
+        require("refactoring").setup()
+
+        require("telescope").load_extension("refactoring")
+        -- vim.keymap.set("x", "<leader>rr", ":Refactor extract ")
+        -- vim.keymap.set("x", "<leader>rf", ":Refactor extract_to_file ")
+
+        -- vim.keymap.set("x", "<leader>rv", ":Refactor extract_var ")
+
+        -- vim.keymap.set({ "n", "x" }, "<leader>ri", ":Refactor inline_var")
+        --
+        -- vim.keymap.set( "n", "<leader>rI", ":Refactor inline_func")
+        --
+        -- vim.keymap.set("n", "<leader>rb", ":Refactor extract_block")
+        -- vim.keymap.set("n", "<leader>rbf", ":Refactor extract_block_to_file")
+
+        -- Refactoring helper
+        vim.keymap.set({"n", "x"}, "<leader>rr",
+            function() require("telescope").extensions.refactoring.refactors()
+        end, { desc = "[R]efacto[r]" })
+    end,
+}
