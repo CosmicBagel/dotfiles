411ce18a5ca685eccbbc6417316e99e967c66166
CosmicBagel <>
2024-04-18 13:02:37 -0600

    added todo and color highlighting

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -1,5 +1,13 @@
 -- This is where all of the plugins that don't need much config go
 return {
-    'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
-    'tpope/vim-surround', -- suround motion
+    "tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
+    "tpope/vim-surround", -- suround motion
+    "norcalli/nvim-colorizer.lua", -- hex codes show their color
+    {
+        -- highlights todos and gives you nice lists
+        "folke/todo-comments.nvim",
+        event = "VimEnter",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        opts = {}
+    },
 }
