d5d285e5413395bf8ce44b295352093128170062
CosmicBagel <>
2024-04-18 22:41:54 -0600

    updated todo

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -5,40 +5,9 @@ require('lazy_init')
 require('autocommands')
 
 -- -- -- config todo -- -- --
--- lazy.nvim
--- treesitter
--- A COLOR SCHEME FOR THE LOVE OF GOD
--- nvim-lspconfig
---      mason, mason-lspconfig,mason-tool-installer
---      note: use autocommand OnAttach for lsp specific keybinds, so when lsp is absent
---      normal vim keybinds will still work
--- cmp
+-- dadbod - allows running sql queries from neovim
+-- rest.nvim - do http calls right from nvim and get the results
 
--- telescope with fzf and telescope-ui-select
--- oil.nvim
--- undotree
--- refactoring.nvim
---
--- indent-blankline -- indentation guidelines
---
--- 'nvim-lua/plenary.nvim',
--- dressing
--- nvim-tree/nvim-web-devicons
--- todo-comments.nvim -- highlight todo notes
--- mini.nvim -- look into the mini plugins
---
-
--- some kind of status bar
--- tpope/vim-sleuth <- Detect tabstop and shiftwidth automatically
--- that code folding plugin (ufo.nvim I think)
--- numToStr/Comment.nvim
-
--- stevearc/conform.nvim --Autoformat
--- gitsigns
--- jump or sneak
 -- https://codeium.com/
 
--- steal things from this guy! https://youtu.be/V070Zmvx9AM
---      lots of little tricks in here
 --  more text objects (https://github.com/nvim-treesitter/nvim-treesitter-textobjects)
--- https://github.com/epwalsh/obsidian.nvim
