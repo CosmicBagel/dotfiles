45389a7f0bdf7954df1395214f0e4adfc36993e0
CosmicBagel <>
2024-04-18 22:39:01 -0600

    added basic framework for debugging, linting, testing

---
+++ config/nvim/lua/plugins/neotest.lua
@@ -0,0 +1,29 @@
+return {
+    "nvim-neotest/neotest",
+    dependencies = {
+        "nvim-neotest/nvim-nio",
+        "nvim-lua/plenary.nvim",
+        "antoinemadec/FixCursorHold.nvim",
+        "nvim-treesitter/nvim-treesitter"
+    },
+    opts = {
+        -- Can be a list of adapters like what neotest expects,
+        -- or a list of adapter names,
+        -- or a table of adapter names, mapped to adapter configs.
+        -- The adapter will then be automatically loaded with the config.
+        adapters = {},
+        -- Example for loading neotest-go with a custom config
+        -- adapters = {
+        --   ["neotest-go"] = {
+        --     args = { "-tags=integration" },
+        --   },
+        -- },
+        status = { virtual_text = true },
+        output = { open_on_run = true },
+        quickfix = {
+            open = function()
+                require("trouble").open({ mode = "quickfix", focus = false })
+            end,
+        },
+    }
+}
