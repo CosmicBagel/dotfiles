a01d2e784f170f6a596409fd68773c9b2fb9295b
CosmicBagel <>
2024-04-19 11:54:07 -0600

    Configured debug setup, now works with cpp projects

--- config/nvim/lua/plugins/debug.lua
+++ config/nvim/lua/plugins/debug.lua
@@ -7,84 +7,116 @@
 -- kickstart.nvim and not kitchen-sink.nvim ;)
 
 return {
-  -- NOTE: Yes, you can install new plugins here!
-  'mfussenegger/nvim-dap',
-  -- NOTE: And you can specify dependencies as well
-  dependencies = {
-    -- Creates a beautiful debugger UI
-    'rcarriga/nvim-dap-ui',
+    -- NOTE: Yes, you can install new plugins here!
+    'mfussenegger/nvim-dap',
+    -- NOTE: And you can specify dependencies as well
+    dependencies = {
+        -- Creates a beautiful debugger UI
+        'rcarriga/nvim-dap-ui',
 
-    -- Required dependency for nvim-dap-ui
-    'nvim-neotest/nvim-nio',
+        -- Required dependency for nvim-dap-ui
+        'nvim-neotest/nvim-nio',
 
-    -- Installs the debug adapters for you
-    'williamboman/mason.nvim',
-    'jay-babu/mason-nvim-dap.nvim',
+        -- Installs the debug adapters for you
+        'williamboman/mason.nvim',
+        'jay-babu/mason-nvim-dap.nvim',
 
-    -- Add your own debuggers here
-    'leoluz/nvim-dap-go',
-  },
-  config = function()
-    local dap = require 'dap'
-    local dapui = require 'dapui'
+        -- Add your own debuggers here
+        'leoluz/nvim-dap-go',
+    },
+    config = function()
+        local dap = require 'dap'
+        local dapui = require 'dapui'
 
-    require('mason-nvim-dap').setup {
-      -- Makes a best effort to setup the various debuggers with
-      -- reasonable debug configurations
-      automatic_setup = true,
+        require('mason-nvim-dap').setup {
+            -- Makes a best effort to setup the various debuggers with
+            -- reasonable debug configurations
+            automatic_installation = true,
 
-      -- You can provide additional configuration to the handlers,
-      -- see mason-nvim-dap README for more information
-      handlers = {},
+            -- You can provide additional configuration to the handlers,
+            -- see mason-nvim-dap README for more information
+            handlers = {
+                function(config)
+                    require('mason-nvim-dap').default_setup(config)
+                end,
+                codelldb = function(config)
+                    local path = vim.fn.stdpath("data") .. "/mason/bin/codelldb"
+                    config.adapters = {
+                        type = "server",
+                        port = "${port}",
+                        executable = {
+                            command = path, -- adjust as needed, must be absolute path
+                            args = { "--port", "${port}" },
+                            -- On windows you may have to uncomment this:
+                            -- detached = false,
+                        },
+                    }
+                    require('mason-nvim-dap').default_setup(config) -- don't forget this!
+                end,
+            },
 
-      -- You'll need to check that you have the required things installed
-      -- online, please don't ask me how to install them :)
-      ensure_installed = {
-        -- Update this to ensure that you have the debuggers for the langs you want
-        'delve',
-      },
-    }
+            -- You'll need to check that you have the required things installed
+            -- online, please don't ask me how to install them :)
+            ensure_installed = {
+                -- Update this to ensure that you have the debuggers for the langs you want
+                'delve',
+                'codelldb',
+            },
+        }
 
-    -- Basic debugging keymaps, feel free to change to your liking!
-    vim.keymap.set('n', '<F5>', dap.continue, { desc = 'Debug: Start/Continue' })
-    vim.keymap.set('n', '<F1>', dap.step_into, { desc = 'Debug: Step Into' })
-    vim.keymap.set('n', '<F2>', dap.step_over, { desc = 'Debug: Step Over' })
-    vim.keymap.set('n', '<F3>', dap.step_out, { desc = 'Debug: Step Out' })
-    vim.keymap.set('n', '<leader>b', dap.toggle_breakpoint, { desc = 'Debug: Toggle Breakpoint' })
-    vim.keymap.set('n', '<leader>B', function()
-      dap.set_breakpoint(vim.fn.input 'Breakpoint condition: ')
-    end, { desc = 'Debug: Set Breakpoint' })
+        -- Basic debugging keymaps, feel free to change to your liking!
+        vim.keymap.set('n', '<leader>bc', function()
+            -- (Re-)reads launch.json if present
+            -- this lets us avoid typing in the path every time
+            if vim.fn.filereadable(".vscode/launch.json") then
+                require("dap.ext.vscode").load_launchjs(nil, {
+                    codelldb = { "c", "cpp", "rust" },
+                    delve = { "go" },
+                })
+            end
+            dap.continue()
+        end, { desc = 'Start/[C]ontinue' })
+        vim.keymap.set('n', '<leader>bq', dap.terminate, { desc = '[Q]uit debug session' })
+        vim.keymap.set('n', '<leader>bs', dap.step_into, { desc = '[S]tep Into' })
+        vim.keymap.set('n', '<leader>bo', dap.step_out, { desc = 'Step [O]ut' })
+        vim.keymap.set('n', '<leader>bv', dap.step_over, { desc = 'Step O[v]er' })
+        vim.keymap.set('n', '<leader>bb', dap.toggle_breakpoint, { desc = 'Toggle [B]reakpoint' })
+        vim.keymap.set('n', '<leader>bB', function()
+            dap.set_breakpoint(vim.fn.input 'Breakpoint condition: ')
+        end, { desc = 'Set Conditional [B]reakpoint' })
 
-    -- Dap UI setup
-    -- For more information, see |:help nvim-dap-ui|
-    dapui.setup {
-      -- Set icons to characters that are more likely to work in every terminal.
-      --    Feel free to remove or use ones that you like more! :)
-      --    Don't feel like these are good choices.
-      icons = { expanded = '▾', collapsed = '▸', current_frame = '*' },
-      controls = {
-        icons = {
-          pause = '⏸',
-          play = '▶',
-          step_into = '⏎',
-          step_over = '⏭',
-          step_out = '⏮',
-          step_back = 'b',
-          run_last = '▶▶',
-          terminate = '⏹',
-          disconnect = '⏏',
-        },
-      },
-    }
+        -- Dap UI setup
+        -- For more information, see |:help nvim-dap-ui|
+        dapui.setup {
+            -- Set icons to characters that are more likely to work in every terminal.
+            --    Feel free to remove or use ones that you like more! :)
+            --    Don't feel like these are good choices.
+            icons = { expanded = '▾', collapsed = '▸', current_frame = '*' },
+            controls = {
+                icons = {
+                    pause = '⏸',
+                    play = '▶',
+                    step_into = '⏎',
+                    step_over = '⏭',
+                    step_out = '⏮',
+                    step_back = 'b',
+                    run_last = '▶▶',
+                    terminate = '⏹',
+                    disconnect = '⏏',
+                },
+            },
+        }
 
-    -- Toggle to see last session result. Without this, you can't see session output in case of unhandled exception.
-    vim.keymap.set('n', '<F7>', dapui.toggle, { desc = 'Debug: See last session result.' })
+        -- Toggle to see last session result. Without this, you can't see session output in case of unhandled exception.
+        vim.keymap.set('n', '<leader>bu', dapui.toggle, { desc = 'Toggle Debug [U]I' })
 
-    dap.listeners.after.event_initialized['dapui_config'] = dapui.open
-    dap.listeners.before.event_terminated['dapui_config'] = dapui.close
-    dap.listeners.before.event_exited['dapui_config'] = dapui.close
+        dap.listeners.after.event_initialized['dapui_config'] = dapui.open
+        dap.listeners.before.event_terminated['dapui_config'] = dapui.close
+        dap.listeners.before.event_exited['dapui_config'] = dapui.close
+        -- dap.listeners.before.disconnect['dapui_config'] = dapui.close
 
-    -- Install golang specific config
-    require('dap-go').setup()
-  end,
+
+        -- Install golang specific config
+        require('dap-go').setup()
+    end,
 }
--- config/nvim/lua/plugins/which_key.lua
+++ config/nvim/lua/plugins/which_key.lua
@@ -6,6 +6,7 @@ return {
 
         -- Document existing key chains
         require('which-key').register {
+            ['<leader>b'] = { name = 'De[b]ug', _ = 'which_key_ignore' },
             ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
             ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
             ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
