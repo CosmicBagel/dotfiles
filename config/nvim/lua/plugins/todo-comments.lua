64f315ca3f1d2984ea9c69f0f2e2e8c11f585aec
CosmicBagel <>
2024-04-20 12:19:47 -0600

    split init.lua into separate files for consistency

---
+++ config/nvim/lua/plugins/todo-comments.lua
@@ -0,0 +1,7 @@
+return {
+    -- highlights todos and gives you nice lists
+    "folke/todo-comments.nvim",
+    event = "VimEnter",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {}
+}
