2214d3a3d208614bac6bbc52160eae41fdc5b310
CosmicBagel <>
2024-04-21 15:00:24 -0600

    Best attempt at getting unity debugging working (its not tho)

--- config/nvim/lua/plugins/debug.lua
+++ config/nvim/lua/plugins/debug.lua
@@ -28,6 +28,48 @@ return {
         local dap = require 'dap'
         local dapui = require 'dapui'
 
+        local vstuc_path = os.getenv("HOME") ..
+            "/.vscode/extensions/visualstudiotoolsforunity.vstuc-1.0.1/bin/"
+        dap.adapters.vstuc = {
+            type = 'executable',
+            command = 'dotnet',
+            args = {
+                'UnityDebugAdapter.dll'
+            },
+            options ={
+                cwd = vstuc_path,
+            },
+        }
+
+        dap.configurations.cs = {
+            {
+                name = 'Unity Debugger',
+                type = 'vstuc',
+                request = 'attach',
+                projectPath = vim.fn.getcwd(),
+                logFile = vim.env.HOME .. "/vstuc.log",
+                endPoint = function()
+                    local system_obj = vim.system(
+                        { "dotnet", vstuc_path .. "UnityAttachProbe.dll" })
+                    local probe_result = system_obj:wait(2000).stdout
+
+                    if probe_result == nil or #probe_result == 0 then
+                        print("No endpoint found (is unity running?)")
+                        return ""
+                    end
+                    local pattern = [["debuggerPort":(%d+)]]
+                    local port = string.match(probe_result, pattern)
+
+                    if port == nil or #port == 0 then
+                        print("Failed to parse debugger port")
+                        return ""
+                    end
+
+                    return "127.0.0.1:" .. port
+                end,
+            }
+        }
+
         require('mason-nvim-dap').setup {
             -- Makes a best effort to setup the various debuggers with
             -- reasonable debug configurations
