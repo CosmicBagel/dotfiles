edc5dc1c87fdb29530a620c259ee3bcfaccc24e6
CosmicBagel <>
2024-04-29 16:54:27 -0600

    Added godot editor

--- aconfmgr/30-aur-package.sh
+++ aconfmgr/30-aur-package.sh
@@ -32,3 +32,6 @@ AddPackage --foreign wlrctl # Utility for miscellaneous wlroots extensions
 
 
 AddPackage --foreign neovim-git # Fork of Vim aiming to improve user experience, plugins, and GUIs
+
+AddPackage --foreign godot-mono-bin # Godot is an advanced, feature packed, multi-platform 2D and 3D game engine. It provides a huge set of common tools, so you can just focus on making your game without reinventing the wheel.
+
