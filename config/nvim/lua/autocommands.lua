c5d18c01da6d0ffbd41021fe2f694aebfdef6587
CosmicBagel <>
2024-04-30 15:32:58 -0600

    Hack for editor staying in insert mode after using telescope

--- config/nvim/lua/autocommands.lua
+++ config/nvim/lua/autocommands.lua
@@ -68,3 +68,15 @@ autocmd({ "InsertLeave" }, {
     pattern = "*",
     command = "set relativenumber"
 })
+
+-- temporary hack to not stay in insert mode after enter a file with telescope
+-- apparently this was fixed, but idk still broken for me
+vim.api.nvim_create_autocmd("WinLeave", {
+  callback = function()
+    if vim.bo.ft == "TelescopePrompt" and vim.fn.mode() == "i" then
+      vim.api.nvim_feedkeys(
+                vim.api.nvim_replace_termcodes( "<Esc>", true, false, true),
+                "i", false)
+    end
+  end,
+})
