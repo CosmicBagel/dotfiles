ecfae105b86e04579a836375fb24f2cc804498b0
CosmicBagel <>
2024-04-30 14:16:27 -0600

    rainbow delimiters has a massive perf hit, esp in zig

--- config/nvim/lua/plugins/rainbow-delimiters.lua
+++ config/nvim/lua/plugins/rainbow-delimiters.lua
@@ -1,3 +1,4 @@
 return {
-    "HiPhish/rainbow-delimiters.nvim", -- rainbow brackets
+    -- disabled due to heavy performance impact
+    -- "HiPhish/rainbow-delimiters.nvim", -- rainbow brackets
 }
