7ff4220d27e09914a7e5b091a3ef5ce5d455280f
CosmicBagel <>
2024-05-03 09:30:45 -0600

    Disabled zig autoformat on save

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -171,9 +171,9 @@ return {
         servers.gopls = {}
         -- pyright = {}
         servers.rust_analyzer = {}
-        servers.zls = {
 
-        }
+        vim.g.zig_fmt_autosave = 0
+        servers.zls = {}
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
