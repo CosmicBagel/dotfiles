a2a9f83df57ddeebc4216060fa7ad2779f80749a
CosmicBagel <>
2024-05-06 22:22:44 -0600

    added show/hide status line commands

--- config/nvim/lua/user_commands.lua
+++ config/nvim/lua/user_commands.lua
@@ -2,7 +2,7 @@
 -- nice when reading help docs
 -- keep the cursor centred (should rewrite as lua)
 vim.api.nvim_create_user_command('CenterCursorEnable',
-[[let &scrolloff=(winheight(win_getid())/2)+1 |
+  [[let &scrolloff=(winheight(win_getid())/2)+1 |
 execute 'normal zz' |
 augroup VCenterCursor |
   au! |
@@ -10,20 +10,30 @@ augroup VCenterCursor |
     let &scrolloff=(winheight(win_getid())/2)+1 |
 augroup END
 ]],
-{})
+  {})
 
 vim.api.nvim_create_user_command('CenterCursorDisable',
-function()
-    vim.api.nvim_clear_autocmds({group = "VCenterCursor"})
-    vim.opt.scrolloff=10
-end, {})
+  function()
+    vim.api.nvim_clear_autocmds({ group = "VCenterCursor" })
+    vim.opt.scrolloff = 10
+  end, {})
 
 vim.api.nvim_create_user_command('SpellfileRegen',
-function()
+  function()
     local path = vim.fn.stdpath('config') .. '/spell'
     vim.cmd('mkspell! ' .. path .. '/en.utf-8.add.spl ' .. path .. '/en.utf-8.add')
-end, {})
+  end, {})
 
 -- common typo when quitting and writing
 vim.api.nvim_create_user_command('Q', 'q', {})
 vim.api.nvim_create_user_command('W', 'w', {})
+
+-- show/hide lualine
+vim.api.nvim_create_user_command('StatusLineHide',
+  function()
+    vim.opt.laststatus = 0
+  end, {})
+vim.api.nvim_create_user_command('StatusLineShow',
+  function()
+    vim.opt.laststatus = 2
+  end, {})
