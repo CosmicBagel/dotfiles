64ee65f90e4f44276539b83dc9ee18f345da2ea8
CosmicBagel <>
2024-07-31 13:45:00 -0600

    Inlay hints finally work

---
+++ config/nvim/lua/plugins/inlay-hints.lua
@@ -0,0 +1,15 @@
+return {
+    "MysticalDevil/inlay-hints.nvim",
+    event = "LspAttach",
+    dependencies = { "neovim/nvim-lspconfig" },
+    keys = {
+        {
+            '<leader>n',
+            "<cmd>InlayHintsToggle<cr>",
+            desc = 'Toggle I[n]lay Hints',
+        },
+    },
+    config = function()
+        require("inlay-hints").setup()
+    end
+}
