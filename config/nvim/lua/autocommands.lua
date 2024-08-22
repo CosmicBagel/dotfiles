4b44668673e51df5b0207fea3f0d9540aac93b69
CosmicBagel <>
2024-08-22 14:46:55 -0600

    Ran autoformat

--- config/nvim/lua/autocommands.lua
+++ config/nvim/lua/autocommands.lua
@@ -2,44 +2,44 @@
 
 local augroup = vim.api.nvim_create_augroup
 local autocmd = vim.api.nvim_create_autocmd
-local CosmicBagelGroup = augroup('CosmicBagel', { clear = true })
+local CosmicBagelGroup = augroup("CosmicBagel", { clear = true })
 
 -- Highlight when yanking (copying) text
 --  Try it with `yap` in normal mode
 --  See `:help vim.highlight.on_yank()`
-autocmd('TextYankPost', {
-    desc = 'Highlight when yanking (copying) text',
-    group = augroup('highlight-yank', { clear = true }),
-    callback = function()
-        vim.highlight.on_yank()
-    end,
+autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = augroup("highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
 })
 
 -- strip trailing spaces on each line
 -- (will not report an error on no match)
 autocmd({ "BufWritePre" }, {
-    group = CosmicBagelGroup,
-    pattern = "*",
-    callback = function()
-        if vim.bo.filetype == "oil" then
-            return
-        end
-        vim.api.nvim_command("normal! mz")
-        vim.api.nvim_command([[%s/\s\+$//e]])
-        vim.api.nvim_command("normal! `z")
-    end,
+	group = CosmicBagelGroup,
+	pattern = "*",
+	callback = function()
+		if vim.bo.filetype == "oil" then
+			return
+		end
+		vim.api.nvim_command("normal! mz")
+		vim.api.nvim_command([[%s/\s\+$//e]])
+		vim.api.nvim_command("normal! `z")
+	end,
 })
 
 -- my translation of
 -- autocmd FileType dap-float nnoremap <buffer><silent> q <cmd>close!<CR>
 -- makes q and esc close popup/floats
 autocmd({ "FileType" }, {
-    group = CosmicBagelGroup,
-    pattern = "dap-float",
-    callback = function()
-        vim.keymap.set("n", "q", "<cmd>close!<CR>", { noremap = true, silent = true, buffer = true })
-        vim.keymap.set("n", "<esc>", "<cmd>close!<CR>", { noremap = true, silent = true, buffer = true })
-    end,
+	group = CosmicBagelGroup,
+	pattern = "dap-float",
+	callback = function()
+		vim.keymap.set("n", "q", "<cmd>close!<CR>", { noremap = true, silent = true, buffer = true })
+		vim.keymap.set("n", "<esc>", "<cmd>close!<CR>", { noremap = true, silent = true, buffer = true })
+	end,
 })
 
 -- disable spell checking for windows and things
@@ -76,22 +76,24 @@ autocmd({ "BufEnter", "BufNew" }, {
 
 -- godot config files
 autocmd({ "BufNewFile", "BufRead" }, {
-    group = CosmicBagelGroup,
-    pattern = { "*.import", "*.godot" },
-    command = "setfiletype gdresource"
+	group = CosmicBagelGroup,
+	pattern = { "*.import", "*.godot" },
+	command = "setfiletype gdresource",
 })
 
 -- relative line numbers in normal mode, but not insert mode
 autocmd({ "InsertEnter" }, {
-    group = CosmicBagelGroup,
-    pattern = "*",
-    command = "set norelativenumber"
+	group = CosmicBagelGroup,
+	pattern = "*",
+	command = "set norelativenumber",
 })
 
 autocmd({ "InsertLeave" }, {
-    group = CosmicBagelGroup,
-    pattern = "*",
-    command = "set relativenumber"
+	group = CosmicBagelGroup,
+	pattern = "*",
+	command = "set relativenumber",
+})
+
 -- basic auto-save
 autocmd({ "BufDelete", "BufLeave" }, {
 	group = CosmicBagelGroup,
@@ -125,12 +127,10 @@ autocmd({ "BufDelete", "BufLeave" }, {
 
 -- temporary hack to not stay in insert mode after enter a file with telescope
 -- apparently this was fixed, but idk still broken for me
-vim.api.nvim_create_autocmd("WinLeave", {
-  callback = function()
-    if vim.bo.ft == "TelescopePrompt" and vim.fn.mode() == "i" then
-      vim.api.nvim_feedkeys(
-                vim.api.nvim_replace_termcodes( "<Esc>", true, false, true),
-                "i", false)
-    end
-  end,
+autocmd("WinLeave", {
+	callback = function()
+		if vim.bo.ft == "TelescopePrompt" and vim.fn.mode() == "i" then
+			vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "i", false)
+		end
+	end,
 })
