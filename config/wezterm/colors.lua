a9ef4ada66143bfb404ae3dd820ccd710281bea8
CosmicBagel <>
2024-08-22 14:55:02 -0600

    Monokai color theme + smaller font

--- config/wezterm/colors.lua
+++ config/wezterm/colors.lua
@@ -1,56 +1,72 @@
-local M = {
-    -- The default text color
-    foreground = '#ffffff',
-    -- The default background color
-    background = '#000000',
+local wezterm = require("wezterm")
 
-    -- the foreground color of selected text
-    selection_fg = '#000000',
-    -- the background color of selected text
-    selection_bg = '#ffffff',
+local me_colors = { -- The default text color foreground = '#ffffff', The default background color
+	background = "#000000",
 
-    -- Visual bell
-    -- visual_bell = '#0b0e14',
-    visual_bell = '#000000',
+	-- the foreground color of selected text
+	selection_fg = "#000000",
+	-- the background color of selected text
+	selection_bg = "#ffffff",
 
-    cursor_bg = '#ffffff',
-    cursor_fg = '#000000',
-    cursor_border = '#ffffff',
-    
-    -- changes cursor color when LEADER key is active
-	compose_cursor = 'orange',
+	-- Visual bell
+	-- visual_bell = '#0b0e14',
+	-- visual_bell = "#000000",
+	visual_bell = "#2b292c",
 
-    ansi = {
-        '#1a1a1a', -- black  
-        '#df0008', -- red    
-        '#98e024', -- green  
-        '#fa8419', -- yellow 
-        '#2179ff', -- blue   
-        '#e745ff', -- magenta
-        '#58d1eb', -- cyan   
-        '#c4c5b5', -- white  
-    },
-    brights = {
-        '#625e4c', -- black   
-        '#df0008', -- red     
-        '#98e024', -- green   
-        '#e0d561', -- yellow  
-        '#2179ff', -- blue    
-        '#e745ff', -- magenta 
-        '#58d1eb', -- cyan    
-        '#f6f6ef', -- white   
-    },
+	cursor_bg = "#ffffff",
+	cursor_fg = "#000000",
+	cursor_border = "#ffffff",
 
-    -- tab bar colors
-    tab_bar = {
-        background = 'rgba(0, 0, 0, 90%)',
+	-- changes cursor color when LEADER key is active
+	compose_cursor = "orange",
 
-        -- The new tab button that let you create new tabs
-        new_tab = {
-          bg_color = 'rgba(51, 51, 51, 100%)',
-          fg_color = '#bbbbbb',
-        },
-    },
+	ansi = {
+		"#1a1a1a", -- black
+		"#df0008", -- red
+		"#98e024", -- green
+		"#fa8419", -- yellow
+		"#2179ff", -- blue
+		"#e745ff", -- magenta
+		"#58d1eb", -- cyan
+		"#c4c5b5", -- white
+	},
+	brights = {
+		"#625e4c", -- black
+		"#df0008", -- red
+		"#98e024", -- green
+		"#e0d561", -- yellow
+		"#2179ff", -- blue
+		"#e745ff", -- magenta
+		"#58d1eb", -- cyan
+		"#f6f6ef", -- white
+	},
+
+	-- tab bar colors
+	tab_bar = {
+		background = "rgba(0, 0, 0, 90%)",
+
+		-- The new tab button that let you create new tabs
+		new_tab = {
+			bg_color = "rgba(51, 51, 51, 100%)",
+			fg_color = "#bbbbbb",
+		},
+	},
 }
+
+local M = {}
+
+M.apply_colors = function(config)
+	local schemename = "MonokaiPro (Gogh)"
+	local scheme = wezterm.get_builtin_color_schemes()[schemename]
+	-- use monokai pro as base, but set background color to black
+	scheme.background = me_colors.background
+	scheme.visual_bell = me_colors.visual_bell
+	scheme.compose_cursor = me_colors.compose_cursor
+
+	config.color_schemes = {
+		[schemename] = scheme,
+	}
+	config.color_scheme = schemename
+end
 
 return M
--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,50 +1,50 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 -- Env
 -- config.term = 'wezterm'
-config.term = 'xterm-256color'
+config.term = "xterm-256color"
 
 -- Font
-config.font = wezterm.font { family = "JetBrains Mono" }
-config.font_size = 14
+config.font = wezterm.font({ family = "JetBrains Mono" })
+config.font_size = 11
 config.cursor_thickness = 0.20
 
 -- Scroll history
 config.scrollback_lines = 1000000
 
 -- Window
-config.initial_rows = 40
-config.initial_cols = 166
-config.window_background_opacity = 0.83
+config.initial_rows = 52
+config.initial_cols = 171
+config.window_background_opacity = 0.996
 config.window_decorations = "NONE"
 config.window_padding = {
-    left = 0,
-    right = 0,
-    top = 0,
-    bottom = 0
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
 }
 
 config.hide_mouse_cursor_when_typing = true
 
 -- Visual bell
 config.visual_bell = {
-  fade_in_function = 'Linear',
-  fade_in_duration_ms = 0,
-  fade_out_function = 'Linear',
-  fade_out_duration_ms = 100,
+	fade_in_function = "Linear",
+	fade_in_duration_ms = 0,
+	fade_out_function = "Linear",
+	fade_out_duration_ms = 100,
 }
 
 -- Tab bar
-require('tabbar').apply_tab_bar_config(config)
+require("tabbar").apply_tab_bar_config(config)
 
 -- Colors
-config.colors = require("colors")
+require("colors").apply_colors(config)
 
 -- Key bindings
 config.enable_kitty_keyboard = true -- needed for neovim keybindings
 config.disable_default_key_bindings = true
-config.leader = { key = 'q', mods = 'CTRL', timeout_milliseconds = 1500 }
+config.leader = { key = "q", mods = "CTRL", timeout_milliseconds = 1500 }
 config.keys = require("keys")
 
 return config
