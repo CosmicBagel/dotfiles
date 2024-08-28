937a3d04805d95632874b4fbb774f1a4e07f39ee
CosmicBagel <>
2024-08-28 00:19:15 -0600

    Added a little command that may be helpful for bootstraping wezterm

--- bashrc_helpers/interactive_functions.sh
+++ bashrc_helpers/interactive_functions.sh
@@ -36,3 +36,30 @@ function yy() {
 	fi
 	rm -f -- "$tmp"
 }
+
+function UpdateWeztermAppImage {
+    local url="https://github.com/wez/wezterm/releases/download/nightly/WezTerm-nightly-Ubuntu20.04.AppImage"
+    local hash_url="https://github.com/wez/wezterm/releases/download/nightly/WezTerm-nightly-Ubuntu20.04.AppImage.sha256"
+
+    echo "Getting hash..."
+    local remote_hash=$(curl -L $hash_url | cut -d' ' -f1)
+    local current_hash=$(sha256sum $HOME/bin/wezterm | cut -d' ' -f1)
+    if [[ $remote_hash == $current_hash ]]; then
+	echo "Wezterm is up to date"
+	return 0
+    fi
+
+    rm $HOME/bin/wezterm_download
+    curl -Lo $HOME/bin/wezterm_download $url
+    if [[ $? -ne 0 ]]; then
+	return 1
+    fi
+
+    local download_hash=$(sha256sum $HOME/bin/wezterm_download | cut -d' ' -f1)
+    if [[ $remote_hash != $download_hash ]]; then
+	echo 'ERROR Hash of download failed to verify'
+	return 1
+    fi
+
+    rm $HOME/bin/wezterm && mv $HOME/bin/wezterm_download $HOME/bin/wezterm && chmod +x $HOME/bin/wezterm
+}
