95867a177306990e4e6e40f3c4b34e8da02614ff
CosmicBagel <>
2024-09-06 19:16:31 -0600

    No more auto center cursor

--- config/nvim/lua/user_commands.lua
+++ config/nvim/lua/user_commands.lua
@@ -1,8 +1,9 @@
 -- -- -- user commands -- -- --
 -- nice when reading help docs
 -- keep the cursor centred (should rewrite as lua)
-vim.api.nvim_create_user_command('CenterCursorEnable',
-  [[let &scrolloff=(winheight(win_getid())/2)+1 |
+vim.api.nvim_create_user_command(
+	"CenterCursorEnable",
+	[[let &scrolloff=(winheight(win_getid())/2)+1 |
 execute 'normal zz' |
 augroup VCenterCursor |
   au! |
@@ -10,31 +11,28 @@ augroup VCenterCursor |
     let &scrolloff=(winheight(win_getid())/2)+1 |
 augroup END
 ]],
-  {})
-vim.cmd.CenterCursorEnable()
+	{}
+)
+-- vim.cmd.CenterCursorEnable()
 
-vim.api.nvim_create_user_command('CenterCursorDisable',
-  function()
-    vim.api.nvim_clear_autocmds({ group = "VCenterCursor" })
-    vim.opt.scrolloff = 10
-  end, {})
+vim.api.nvim_create_user_command("CenterCursorDisable", function()
+	vim.api.nvim_clear_autocmds({ group = "VCenterCursor" })
+	vim.opt.scrolloff = 10
+end, {})
 
-vim.api.nvim_create_user_command('SpellfileRegen',
-  function()
-    local path = vim.fn.stdpath('config') .. '/spell'
-    vim.cmd('mkspell! ' .. path .. '/en.utf-8.add.spl ' .. path .. '/en.utf-8.add')
-  end, {})
+vim.api.nvim_create_user_command("SpellfileRegen", function()
+	local path = vim.fn.stdpath("config") .. "/spell"
+	vim.cmd("mkspell! " .. path .. "/en.utf-8.add.spl " .. path .. "/en.utf-8.add")
+end, {})
 
 -- common typo when quitting and writing
-vim.api.nvim_create_user_command('Q', 'q', {})
-vim.api.nvim_create_user_command('W', 'w', {})
+vim.api.nvim_create_user_command("Q", "q", {})
+vim.api.nvim_create_user_command("W", "w", {})
 
 -- show/hide lualine
-vim.api.nvim_create_user_command('StatusLineHide',
-  function()
-    vim.opt.laststatus = 0
-  end, {})
-vim.api.nvim_create_user_command('StatusLineShow',
-  function()
-    vim.opt.laststatus = 2
-  end, {})
+vim.api.nvim_create_user_command("StatusLineHide", function()
+	vim.opt.laststatus = 0
+end, {})
+vim.api.nvim_create_user_command("StatusLineShow", function()
+	vim.opt.laststatus = 2
+end, {})
