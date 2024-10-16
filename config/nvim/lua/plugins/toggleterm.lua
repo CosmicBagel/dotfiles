b5b86946cd77abff7762fa8aafe4857d622c3f1a
CosmicBagel <>
2024-10-16 06:47:09 -0600

    Toggle term vertical split size now 50%

--- config/nvim/lua/plugins/toggleterm.lua
+++ config/nvim/lua/plugins/toggleterm.lua
@@ -1,19 +1,29 @@
 return {
-    'akinsho/toggleterm.nvim',
-    version = "*",
-    opts = {
-        direction = 'horizontal',
-        size = function(term)
-            if term.direction == "horizontal" then
-                return vim.o.lines * 0.33
-            elseif term.direction == "vertical" then
-                return vim.o.columns * 0.55
-            end
-        end,
-    },
-    config = function(_, opts)
-        require("toggleterm").setup(opts)
-        vim.keymap.set({"n", "t", "i", "v", "x", "o"}, [[<c-\>]], "<cmd>ToggleTerm direction=vertical<cr>", { noremap = true, silent = true })
-        vim.keymap.set({"n", "t", "i", "v", "x", "o"}, [[<c-`>]], "<cmd>ToggleTerm direction=horizontal<cr>", { noremap = true, silent = true })
-    end
+	"akinsho/toggleterm.nvim",
+	version = "*",
+	opts = {
+		direction = "horizontal",
+		size = function(term)
+			if term.direction == "horizontal" then
+				return vim.o.lines * 0.33
+			elseif term.direction == "vertical" then
+				return vim.o.columns * 0.50
+			end
+		end,
+	},
+	config = function(_, opts)
+		require("toggleterm").setup(opts)
+		vim.keymap.set(
+			{ "n", "t", "i", "v", "x", "o" },
+			[[<c-\>]],
+			"<cmd>ToggleTerm direction=vertical<cr>",
+			{ noremap = true, silent = true }
+		)
+		vim.keymap.set(
+			{ "n", "t", "i", "v", "x", "o" },
+			[[<c-`>]],
+			"<cmd>ToggleTerm direction=horizontal<cr>",
+			{ noremap = true, silent = true }
+		)
+	end,
 }
