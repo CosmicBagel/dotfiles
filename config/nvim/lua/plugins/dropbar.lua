5ff319f97fdc0775b26502078d40be1a2f4a8e4a
CosmicBagel <>
2024-10-26 10:17:11 -0600

    Added dropbar to show breadcrumbs

---
+++ config/nvim/lua/plugins/dropbar.lua
@@ -0,0 +1,8 @@
+return {
+	"Bekaboo/dropbar.nvim",
+	tag = "v8.6.1", -- pinning to aug 2024 release as TS source isn't working in main
+	-- optional, but required for fuzzy finder support
+	dependencies = {
+		"nvim-telescope/telescope-fzf-native.nvim",
+	},
+}
