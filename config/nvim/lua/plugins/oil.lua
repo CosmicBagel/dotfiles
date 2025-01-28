e1caf88366c995dc27afd49b9af42822313ace3b
CosmicBagel <>
2025-01-28 15:34:15 -0700

    nvim - oil - adjusted keybinds so that you can hop into the previewed doc

--- config/nvim/lua/plugins/oil.lua
+++ config/nvim/lua/plugins/oil.lua
@@ -1,28 +1,29 @@
 return {
-    'stevearc/oil.nvim', -- file browser, like vim vinegar
-    opts = {
-        keymaps = {
-            ["q"] = "actions.close",
-        },
-        view_options = {
-            show_hidden = true,
-        },
-        float = {
-            -- Padding around the floating window
-            padding = 2,
-            max_width = 155,
-            max_height = 40,
-            border = "rounded",
-        },
-    },
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function(_, opts)
-        require("oil").setup(opts)
-        vim.keymap.set(
-            "n", "<leader>-", "<CMD>Oil<CR>",
-            { desc = "Open parent directory" }
-        )
-        -- alias for Ex to oil cuz habits
-        vim.api.nvim_create_user_command('Ex', 'Oil', {})
-    end,
+	"stevearc/oil.nvim", -- file browser, like vim vinegar
+	opts = {
+		keymaps = {
+			["?"] = { "actions.show_help", mode = "n" },
+			["q"] = "actions.close",
+			["<C-c>"] = false,
+			["<C-l>"] = false,
+		},
+		view_options = {
+			show_hidden = true,
+		},
+		float = {
+			-- Padding around the floating window
+			padding = 2,
+			max_width = 155,
+			max_height = 40,
+			border = "rounded",
+		},
+	},
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function(_, opts)
+		require("oil").setup(opts)
+		vim.keymap.set("n", "<leader>-", "<CMD>Oil<CR>", { desc = "Oil - Open parent directory" })
+		vim.keymap.set("n", "<leader>_", "<CMD>Oil .<CR>", { desc = "Oil - Open current working directory" })
+		-- alias for Ex to oil cuz habits
+		vim.api.nvim_create_user_command("Ex", "Oil", {})
+	end,
 }
