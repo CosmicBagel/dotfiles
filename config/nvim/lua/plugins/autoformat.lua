516f31ffdee0a5ff767d85e76d7be15a860f5979
CosmicBagel <>
2025-05-21 20:25:14 -0600

    nvim - disabled autoformat on save

--- config/nvim/lua/plugins/autoformat.lua
+++ config/nvim/lua/plugins/autoformat.lua
@@ -18,11 +18,11 @@ return {
 		default_format_opts = {
 			lsp_format = "fallback",
 		},
-		format_on_save = {
-			-- I recommend these options. See :help conform.format for details.
-			lsp_format = "fallback",
-			timeout_ms = 500,
-		},
+		-- format_on_save = {
+		-- 	-- I recommend these options. See :help conform.format for details.
+		-- 	lsp_format = "fallback",
+		-- 	timeout_ms = 500,
+		-- },
 		-- function(bufnr)
 		--     -- Disable "format_on_save lsp_fallback" for languages that don't
 		--     -- have a well standardized coding style. You can add additional
