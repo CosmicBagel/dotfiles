8684caeca47820e4fd8127ca8a756041f8786a31
CosmicBagel <>
2025-06-20 17:28:18 -0600

    nvim - color tweaking

--- config/nvim/lua/plugins/color_scheme.lua
+++ config/nvim/lua/plugins/color_scheme.lua
@@ -64,7 +64,7 @@ return {
 			vim.api.nvim_set_hl(0, "TSComment", { fg = "#c1c0c0" })
 			vim.api.nvim_set_hl(0, "@string.documentation", { fg = "#c1c0c0" })
 
-			vim.api.nvim_set_hl(0, "CursorLineNR", { bg = "#c1c0c0", fg = "#221f22" })
+			vim.api.nvim_set_hl(0, "CursorLineNR", { bg = "#939293", fg = "#221f22" })
 			vim.api.nvim_set_hl(0, "CursorLine", { bg = "#403e41" })
 			vim.api.nvim_set_hl(0, "ColorColumn", { bg = "#ff6188" })
 			-- vim.api.nvim_set_hl(0, "Normal", { bg = "#000000" })
@@ -79,6 +79,7 @@ return {
 			vim.api.nvim_set_hl(0, "ScrollbarCursorHandle", { bg = "#fcfcfa", fg = "#fcfcfa" })
 
 			vim.api.nvim_set_hl(0, "FlashBackdrop", { bg = "none", fg = "#ab9df2" })
+			vim.api.nvim_set_hl(0, "FlashCurrent", { bg = "#ab9df2", fg = "#221f22" })
 			vim.api.nvim_set_hl(0, "FlashLabel", { bg = "none", fg = "#ff6188" })
 			vim.api.nvim_set_hl(0, "LightBulbNumber", { bg = "#ffd866", fg = "#221f22" })
 
