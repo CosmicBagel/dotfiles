af561938c8eafac1e52887d9a9d36d6b96759b6d
CosmicBagel <>
2025-07-08 13:54:41 -0600

    nvim - lualine show git signs, line endings not as icons

--- config/nvim/lua/plugins/lualine.lua
+++ config/nvim/lua/plugins/lualine.lua
@@ -17,14 +17,34 @@ return {
 			"quickfix",
 			"trouble",
 			"toggleterm",
+			"nvim-tree",
 		},
 		sections = {
+			lualine_b = {
+				{ "b:gitsigns_head", icon = "" },
+				{
+					"diff",
+					source = function()
+						local gitsigns = vim.b.gitsigns_status_dict
+						if gitsigns then
+							return {
+								added = gitsigns.added,
+								modified = gitsigns.changed,
+								removed = gitsigns.removed,
+							}
+						end
+					end,
+					symbols = { added = " ", modified = "󰥛 ", removed = " " },
+				},
+				"diagnostics",
+			},
 			lualine_c = {
+				"filename",
 				{
 					function()
 						return require("dap").status()
 					end,
-					icon = { "", color = { fg = "#e7c664" } }, -- nerd icon.
+					icon = { " ", color = { fg = "#e7c664" } }, -- nerd icon.
 					cond = function()
 						if not package.loaded.dap then
 							return false
@@ -33,6 +53,19 @@ return {
 						return session ~= nil
 					end,
 				},
+			},
+			lualine_x = {
+				"encoding",
+				{
+					"fileformat",
+					icons_enabled = true,
+					symbols = {
+						unix = "LF",
+						dos = "CRLF",
+						mac = "CR",
+					},
+				},
+				"filetype",
 			},
 		},
 	},
