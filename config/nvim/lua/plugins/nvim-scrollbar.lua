49ddc0c997cb1b7d3e55fed5e38a56cb685933b8
CosmicBagel <>
2025-07-23 10:47:41 -0600

    nvim - scrollbar - don't show in the little lsp hover windows

--- config/nvim/lua/plugins/nvim-scrollbar.lua
+++ config/nvim/lua/plugins/nvim-scrollbar.lua
@@ -4,6 +4,10 @@ return {
 	config = function(opts)
 		require("scrollbar").setup({
 			set_highlights = false,
+			excluded_buftypes = {
+				"terminal",
+				"nofile", -- don't put scrollbar in little popup windows
+			},
 		})
 		require("scrollbar.handlers.gitsigns").setup()
 	end,
