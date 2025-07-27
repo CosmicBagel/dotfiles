7c00ea1ef81816145863141c067d1ceb41da6ef8
CosmicBagel <>
2025-07-26 21:35:48 -0600

    helix - added config

--- deploy_symlinks_zsh.sh
+++ deploy_symlinks_zsh.sh
@@ -42,6 +42,7 @@ symlinks_dic[$HOME/dotfiles/config/aerospace]="$HOME/.config/aerospace"
 symlinks_dic[$HOME/dotfiles/config/karabiner/]="$HOME/.config/karabiner"
 symlinks_dic[$HOME/dotfiles/config/linearmouse/]="$HOME/.config/linearmouse"
 symlinks_dic[$HOME/dotfiles/config/yt-dlp/]="$HOME/.config/yt-dlp"
+symlinks_dic[$HOME/dotfiles/config/helix/]="$HOME/.config/helix"
 symlinks_dic[$HOME/dotfiles/config/kdiff3rc]="$HOME/Library/Preferences/kdiff3rc"
 
 symlinks_dic[$HOME/dotfiles/config/omnisharp]="$HOME/.omnisharp"
