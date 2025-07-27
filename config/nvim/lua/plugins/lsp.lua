7a0d3cf5334a3e3578abb15870354e5c883c1f06
CosmicBagel <>
2025-07-26 23:45:37 -0600

    nvim - lsp - fixed deprecated usage of sign_define

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -20,10 +20,16 @@ return {
 		})
 
 		-- nice icons for gutter elements
-		vim.fn.sign_define("DiagnosticSignError", { text = "", texthl = "DiagnosticSignError" })
-		vim.fn.sign_define("DiagnosticSignWarn", { text = "", texthl = "DiagnosticSignWarn" })
-		vim.fn.sign_define("DiagnosticSignInfo", { text = "", texthl = "DiagnosticSignInfo" })
-		vim.fn.sign_define("DiagnosticSignHint", { text = "", texthl = "DiagnosticSignHint" })
+		vim.diagnostic.config({
+			signs = {
+				text = {
+					[vim.diagnostic.severity.ERROR] = " ",
+					[vim.diagnostic.severity.WARN] = " ",
+					[vim.diagnostic.severity.INFO] = " ",
+					[vim.diagnostic.severity.HINT] = " ",
+				},
+			},
+		})
 
 		-- give LspInfo window nicer border
 		local win = require("lspconfig.ui.windows")
