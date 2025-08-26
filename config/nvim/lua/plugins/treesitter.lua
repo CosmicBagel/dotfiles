92625de77cd8c2428a53243830f2f1c4a4fb3111
CosmicBagel <>
2025-08-25 18:37:52 -0600

    nvim - treesitter - disabling for files that can be very large (eg json ts handles large files poorly)

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -47,13 +47,14 @@ return {
 
 		highlight = {
 			enable = true,
+			disable = { "json", "markdown", "csv" },
 
 			-- Setting this to true will run `:h syntax` and tree-sitter at the
 			-- same time. Set this to `true` if you depend on "syntax" being
 			-- enabled (like for indentation). Using this option may slow down
 			-- your editor, and you may see some duplicate highlights. Instead
 			-- of true it can also be a list of languages
-			additional_vim_regex_highlighting = false,
+			additional_vim_regex_highlighting = { "json", "markdown", "csv" },
 			-- markdown and ruby may work better with vim highlighting enabled
 		},
 	},
