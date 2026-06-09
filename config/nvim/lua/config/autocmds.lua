2881cd7c9e7a744e54a9b7b4e54940bca7777069
CosmicBagel <854116+CosmicBagel@users.noreply.github.com>
2026-06-09 15:17:24 -0600

    nvim - bringing back my autosave autocommand

--- config/nvim/lua/config/autocmds.lua
+++ config/nvim/lua/config/autocmds.lua
@@ -10,3 +10,34 @@
 local augroup = vim.api.nvim_create_augroup
 local autocmd = vim.api.nvim_create_autocmd
 local CosmicBagelGroup = augroup("CosmicBagel", { clear = true })
+
+-- basic auto-save
+autocmd({ "BufDelete", "BufLeave", "TextChanged", "InsertLeave" }, {
+	group = CosmicBagelGroup,
+	pattern = "*",
+	callback = function(args)
+		local buf = args.buf
+		-- ignore non-modifiable files
+		if vim.fn.getbufvar(buf, "&modifiable") == 0 then
+			return
+		end
+
+		-- don't auto-save oil buffer
+		if vim.bo.filetype == "oil" then
+			return
+		end
+
+		-- don't auto-save if not modified
+		if not vim.api.nvim_get_option_value("modified", { buf = buf }) then
+			return
+		end
+
+		-- don't auto-save if buffer has no name
+		local name = vim.api.nvim_buf_get_name(buf)
+		if name == nil or string.len(name) == 0 then
+			return
+		end
+
+		vim.api.nvim_command("silent! write")
+	end,
+})
