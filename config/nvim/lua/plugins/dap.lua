c234445cadb371973f37dfa06783c82e3f70c129
CosmicBagel <854116+CosmicBagel@users.noreply.github.com>
2026-08-10 12:00:25 -0600

    nivm - lazyvim - dap - removed unnecessary moodle specific config now that I had launch.json working

--- config/nvim/lua/plugins/dap.lua
+++ config/nvim/lua/plugins/dap.lua
@@ -3,17 +3,6 @@ return {
 		"mfussenegger/nvim-dap",
 		opts = function()
 			local dap = require("dap")
-			dap.configurations.php = {
-				{
-					type = "php",
-					request = "launch",
-					name = "Listen for Xdebug (moodle)",
-					port = 9003,
-					pathMappings = {
-						["/var/www/moodle"] = "${workspaceFolder}",
-					},
-				},
-			}
 		end,
 	},
 }
